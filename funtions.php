baf49b9d3ddda1549f1018cdfef71797738a743c
nikhil-nagar <nagarnikhil888@gmail.com>
2024-05-31 16:43:22 +0530

    updated funtions.php file

--- funtions.php
+++ funtions.php
@@ -6,3 +6,13 @@
 * @package triptheme
 */
 
+function triptheme_enqueue_scripts()  {
+
+    wp_register_style('style-css',get_template_directory_uri()."/style.css", [] , filemtime(get_template_directory().'/style.css'), 'all');
+    wp_register_script('script-js',get_template_directory_uri()."/assets/script.js", [] , filemtime(get_template_directory().'/assets/script.js'), 'all');
+
+    wp_enqueue_style('style-css');
+    wp_enqueue_script('script-js');
+}
+
+add_action('wp_enqueue_scripts','triptheme_enqueue_scripts');
